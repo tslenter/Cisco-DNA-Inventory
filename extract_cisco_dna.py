423279cab1071db49034a1879c22672cf6b137d0
Tom Slenter <tomslenter@gmail.com>
2021-07-08 20:56:43 +0200

    Add files via upload

---
+++ extract_cisco_dna.py
@@ -0,0 +1,70 @@
+#Import modules
+import requests
+import os
+from requests.auth import HTTPBasicAuth
+import urllib3
+from prettytable import PrettyTable
+import argparse
+import json
+
+#Disable HTTPS validation
+urllib3.disable_warnings()
+
+#Set variables to None
+hostname = None
+username = None
+password = None
+
+#Global information
+print("Script is created by Remote Syslog")
+print('Running from directory: ', os.getcwd())
+
+#Add arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-n', '--hostname',  help='Enter a hostname or ip of the Cisco DNA Controller', required=True)
+parser.add_argument('-u','--username', help='Add a username', required=True)
+parser.add_argument('-p', '--password', help='Add a password', required=True)
+args = parser.parse_args()
+
+#Extract variables from namespace to global
+globals().update(vars(args))
+
+#Create table
+dnac_devices = PrettyTable(['Hostname','Platform Id','Software Type','Software Version','Up Time', 'Serial Nu', 'MGMT IP' ])
+dnac_devices.padding_width = 1
+
+#Create HTTP header
+headers = {
+              'content-type': "application/json",
+              'x-auth-token': ""
+          }
+#Generate token for DNA Controller
+def dnac_login(host, user, passw):
+    # Generate token
+    BASE_URL = 'https://' + host
+    AUTH_URL = '/dna/system/api/v1/auth/token'
+    USERNAME = user
+    PASSWORD = passw
+
+    response = requests.post(BASE_URL + AUTH_URL, auth=HTTPBasicAuth(USERNAME, PASSWORD), verify=False)
+    token = response.json()['Token']
+    return token
+
+#Extract data from DNA controller
+def network_device_list(token, host):
+    url = "https://" + host + "/api/v1/network-device"
+    headers["x-auth-token"] = token
+    response = requests.get(url, headers=headers, verify=False)
+    data = response.json()
+    for item in data['response']:
+        dnac_devices.add_row([item["hostname"],item["platformId"],item["softwareType"],item["softwareVersion"],item["upTime"], item["serialNumber"], item["managementIpAddress"]])
+
+#Login to DNA Controller
+if hostname or username or password != None:
+    print("Started session on: " + hostname)
+    print("Started session with user: " + username)
+    login = dnac_login(hostname, username, password)
+    network_device_list(login, hostname)
+    print(dnac_devices)
+else:
+    print("Did you use the parameters to run this command?")
